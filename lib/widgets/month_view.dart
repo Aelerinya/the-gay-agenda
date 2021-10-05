030e2fa97d98afa5182f39297541c4623ed81609
Lucie Philippon <aelerinya@gmail.com>
2021-10-05 17:29:32 +0200

    feat: Add screen showing the events on a day

--- lib/widgets/month_view.dart
+++ lib/widgets/month_view.dart
@@ -1,5 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:the_gay_agenda/screens/day_events.dart';
 import 'package:the_gay_agenda/services/events.dart';
 import 'package:the_gay_agenda/utils/datetime_helpers.dart';
 
@@ -38,33 +40,42 @@ class _MonthViewState extends State<MonthView> {
   Widget dayNumber({required DateTime date, required List<Event> events}) {
     final color = date.month == _month.month ? Colors.black : Colors.grey;
     final eventBullet = Padding(
-        padding: EdgeInsets.all(1),
+        padding: const EdgeInsets.all(1),
         child: Icon(
           Icons.circle,
           size: 5,
           color: color,
         ));
-    return Column(children: [
-      const Spacer(),
-      Text(
-        "${date.day}",
-        style: TextStyle(color: color),
-      ),
-      Expanded(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                  events.length.clamp(0, 3), (index) => eventBullet)),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                  (events.length - 3).clamp(0, 3), (index) => eventBullet))
-        ],
-      ))
-    ]);
+    return OpenContainer(
+        // onTap: () => Navigator.of(context)
+        //     .push(DayEventsScreen(date: date, events: events)),
+        closedElevation: 0,
+        openBuilder: (_, __) => DayEventsScreen(
+              date: date,
+              events: events,
+              locale: MaterialLocalizations.of(context),
+            ),
+        closedBuilder: (_, __) => Column(children: [
+              const Spacer(),
+              Text(
+                "${date.day}",
+                style: TextStyle(color: color),
+              ),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                          events.length.clamp(0, 3), (index) => eventBullet)),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate((events.length - 3).clamp(0, 3),
+                          (index) => eventBullet))
+                ],
+              ))
+            ]));
   }
 
   @override
