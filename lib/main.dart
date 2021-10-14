1ecc1c5c854b6e17df2d4bc5c20e79345a64d139
Maëlys Bras de fer <bastien.bras-de-fer@epitech.eu>
2021-10-14 16:47:45 +0200

    refactor: Remove sqflite database

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:the_gay_agenda/screens/home_screen.dart';
-import 'package:the_gay_agenda/services/database.dart';
-import 'package:the_gay_agenda/services/event_model.dart';
-
-import 'constants/example_events.dart';
 
 void main() async {
-  // TODO: remove this later
-  final database = await EventDatabase.create();
-  final events = await database.get();
-  if (events.isEmpty) {
-    await database.insertList(exampleEvents);
-  }
-
-  runApp(ChangeNotifierProvider.value(
-    value: EventModel(database: database),
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:the_gay_agenda/services/event_model.dart';
 import 'package:the_gay_agenda/widgets/month_view.dart';
-import 'package:provider/provider.dart';
+
+import 'package:the_gay_agenda/constants/example_events.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final events = context.watch<EventModel>().events;
     return Scaffold(
       appBar: AppBar(
         title: const Text("The Gay Agenda"),
       ),
-      body: events != null
-          ? MonthView(events: events)
-          : const CircularProgressIndicator(),
+      body: MonthView(events: exampleEvents),
     );
   }
 }
