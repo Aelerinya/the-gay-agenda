fa60e028f54472d9704c49912a5c64e48d4bee6a
Gaël Dorckel <gael2.dorckel@epitech.eu>
2021-10-16 16:32:56 +0200

    feat: added form to settings page

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:the_gay_agenda/widgets/profile_drawer.dart';
+import 'package:the_gay_agenda/widgets/profile_form.dart';
 
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
@@ -9,10 +10,10 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ananias'),
+        title: const Text('The Gay Agenda'),
       ),
       drawer: const ProfileDrawer(),
-      body: const Text('fff'),
+      body: const ProfileForm(),
     );
   }
 }
---
+++ lib/widgets/profile_form.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProfileForm extends StatefulWidget {
+  const ProfileForm({Key? key}) : super(key: key);
+
+  @override
+  _ProfileFormState createState() => _ProfileFormState();
+}
+
+class User {
+  String firstName = '';
+  String lastName = '';
+  String email = '';
+  String photoPath = '';
+
+  save() {
+    print(this);
+  }
+}
+
+class _ProfileFormState extends State<ProfileForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _user = User();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Builder(
+                builder: (context) => Form(
+                    key: _formKey,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'First name'),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please entre your first name';
+                              }
+                            },
+                            onSaved: (value) =>
+                                setState(() => _user.firstName = value!),
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Last name'),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please entre your last name';
+                              }
+                            },
+                            onSaved: (value) =>
+                                setState(() => _user.lastName = value!),
+                          ),
+                          TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Email'),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please entre your email';
+                              }
+                            },
+                            onSaved: (value) =>
+                                setState(() => _user.email = value!),
+                          ),
+                          Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 16.0),
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    final form = _formKey.currentState;
+                                    if (form!.validate()) {
+                                      form.save();
+                                      _user.save();
+                                      _showDialog(context);
+                                    }
+                                  },
+                                  child: const Text('Save'))),
+                        ])))));
+  }
+
+  _showDialog(BuildContext context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text('Submitting form')));
+  }
+}
