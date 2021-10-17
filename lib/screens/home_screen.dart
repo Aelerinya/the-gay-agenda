171f0642f4510dd439ed21ab330fd960d7c3233a
Ersikan <julien.philippon@epitech.eu>
2021-10-17 18:38:25 +0200

    feat: Add create event form (#6)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:the_gay_agenda/services/events.dart';
+import 'package:the_gay_agenda/screens/create_event.dart';
 import 'package:the_gay_agenda/widgets/month_view.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,9 +11,17 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("The Gay Agenda")),
-        body: ValueListenableBuilder(
-            valueListenable: Hive.box<Event>("events").listenable(),
-            builder: (_, Box<Event> box, __) => MonthView(events: box.values)));
+      appBar: AppBar(title: const Text("The Gay Agenda")),
+      body: ValueListenableBuilder(
+          valueListenable: Hive.box<Event>("events").listenable(),
+          builder: (_, Box<Event> box, __) => MonthView(events: box.values)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const CreateEvent()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
